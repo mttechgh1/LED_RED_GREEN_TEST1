b465370c261370335bf198d8e4733c4fc50e26b1
mttechgh1 <140465334+mttechgh1@users.noreply.github.com>
2025-08-22 22:28:48 -0400

    Update LED_GR_TEST.py
    copied working code to repository

--- LED_GR_TEST.py
+++ LED_GR_TEST.py
@@ -1,2 +1,12 @@
-from gpiozero import LED
+# LED test program for raspberry pi 400
+from  gpiozero import LED
 from time import sleep
+I=0
+ledg=LED(25)
+ledr = LED(12)
+#input statement
+ledg.on()
+sleep(3)
+ledr.on()
+speep = 10
+print ("END OF LOOP")
